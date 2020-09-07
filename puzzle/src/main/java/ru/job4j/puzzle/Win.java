83ef37a4a5ecd6c808c278a1c9c96ffe4161b25a
rpegorov <rostislav.egorov@gmail.com>
2020-09-07 12:20:56 +0300

    Сокобан.

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -2,7 +2,30 @@ package ru.job4j.puzzle;
 
 public class Win {
     public static boolean check(int[][] board) {
-        boolean rsl = true;
-        return rsl;
+        boolean result = false;
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][i] == 1) {
+                result = monoHorizontal(board, i) || monoVertical(board, i);
+                break;
+            }
+        }
+        return result;
+    }
+    public static boolean monoHorizontal(int[][] board, int row) {
+        for (int i = 0; i < board[row].length; i++) {
+            if (board[row][i] != 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean monoVertical(int[][] board, int column) {
+        for (int i = 0; i < board[column].length; i++) {
+            if (board[i][column] != 1) {
+                return false;
+            }
+        }
+        return true;
     }
 }
